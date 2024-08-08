47b249da8680d4e1771a54786817c6f873fb3bbd
Victoria <oluwapelumi.ajuwon@gmail.com>
2024-08-08 12:01:30 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,6 +10,7 @@ import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//Add your firebase configuration here
 
 
 // Initialize Firebase
